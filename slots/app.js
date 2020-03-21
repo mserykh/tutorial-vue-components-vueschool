31ee3bf48a8046429653106ad874675b1d0aa0ad
Marie Serykh <marie.serykh@gmail.com>
2020-03-21 18:55:16 +0100

    Created a component `todo-item` and passed HTML to it with slots

--- slots/app.js
+++ slots/app.js
@@ -1,5 +1,14 @@
 Vue.config.devtools = true;
 
+Vue.component('todo-item', {
+    template: '#todo-item-template',
+    data () {
+        return {
+            completed:false
+        }
+    }
+})
+
 new Vue ({
     el: '#app'
 })
